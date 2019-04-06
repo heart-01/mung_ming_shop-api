a29dfc8cc5e19970654624b448a67d9e98e8d379
siwat jomewattana <s1104300051612@gmail.com>
2019-04-06 14:01:57 +0700

    เพิ่ม api เพิ่ม ลบ แก้ไข

--- src/routes.php
+++ src/routes.php
@@ -32,3 +32,50 @@ $app->get('/ShowCustomer/searching/[{query}]',function($request,$response,$args)
     return $this->response->withJson($customer);
 });
 
+//ฟังชั่น api เพิ่มข้อมูล
+$app->post('/AddCustomer',function($request,$response){
+    $input=$request->getParsedBody(); //รับการส่งข้อมูลจาก Body
+    $sql="INSERT INTO customer (ID_customer, ID_status, name_customer, tel_customer, username_customer,password_customer) 
+    VALUES (:ID_customer,:ID_status,:name_customer,:tel_customer,:username_customer,:password_customer)";
+    $sth=$this->db->prepare($sql);
+    $sth->bindParam("ID_customer",$input['ID_customer']);  //$input[ข้างในนี้คือค่า name ที่ส่งมาจากฟร์อม]
+    $sth->bindParam("ID_status",$input['ID_status']);
+    $sth->bindParam("name_customer",$input['name_customer']);
+    $sth->bindParam("tel_customer",$input['tel_customer']);
+    $sth->bindParam("username_customer",$input['username_customer']);
+    $sth->bindParam("password_customer",$input['password_customer']);
+    $sth->execute();
+    
+    $input['id']=$this->db->lastInsertID(); //แสดงข้อมูล auto id ที่เราได้เพิ่มล่าสุด ถ้าใน ฐานข้อมูลไม่มี auto number ไม่ต้องใส่
+    $result=array('msg'=>true); //แสดง massage true
+    return $this->response->withJson($result); //แสดงผลตัวแปร result มาเป็น Json
+});
+
+//ฟังชั่น api delete
+$app->delete('/DelCustomer/[{id}]',function($request,$response,$args){
+    $sth=$this->db->prepare("DELETE FROM customer WHERE ID_customer=:id");
+    $sth->bindParam("id",$args['id']);
+    $sth->execute();
+    
+    $result=array('msg'=>true); //แสดง massage true
+    return $this->response->withJson($result);
+});
+
+//ฟั่งชั่น api update
+$app->post('/UpdateCustomer/[{id}]',function($request,$response,$args){
+    $input=$request->getParsedBody();
+    //return $this->response->withJson($input);
+    $sql="UPDATE customer SET ID_status=:ID_status,name_customer=:name_customer,tel_customer=:tel_customer,username_customer=:username_customer,password_customer=:password_customer 
+    WHERE ID_customer=:id";
+    $sth=$this->db->prepare($sql);
+    $sth->bindParam("id",$args['id']);
+    $sth->bindParam("ID_status",$input['ID_status']); //$input[ข้างในนี้คือค่า name ที่ส่งมาจากฟร์อม]
+    $sth->bindParam("name_customer",$input['name_customer']);
+    $sth->bindParam("tel_customer",$input['tel_customer']);
+    $sth->bindParam("username_customer",$input['username_customer']);
+    $sth->bindParam("password_customer",$input['password_customer']);
+    $sth->execute();
+
+    $result=$args['id']; //return id ของ row ที่ถูก update
+    return $this->response->withJson($result); //แสดงผลตัวแปร result มาเป็น Json
+});
